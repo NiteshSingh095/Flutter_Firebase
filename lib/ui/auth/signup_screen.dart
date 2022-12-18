cf25e488f117eba95646e8ae8c0ef7d0e7437b42
Nitesh Kumar Singh <anshukumargautam095@gmail.com>
2022-12-18 12:40:20 +0530

    SignUp screen added

---
+++ lib/ui/auth/signup_screen.dart
@@ -0,0 +1,155 @@
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/material.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({Key? key}) : super(key: key);
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sign Up Screen"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.only(top: 15.0, left: 10.0, right: 10.0),
+        child: ListView(
+          children: <Widget>[
+            const Padding(
+              padding: EdgeInsets.only(top: 15.0, bottom: 4.0),
+              child: Text(
+                "Welcome,",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40,
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 15.0, bottom: 4.0),
+              child: Text(
+                "Sign Up to continue",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 100.0, bottom: 4.0),
+                    child: TextFormField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        prefixIcon: Icon(Icons.email),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        else if(!EmailValidator.validate(value, true))
+                        {
+                          return 'Invald email address';
+                        }
+                        else
+                        {
+                          return ' ';
+                        }
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 20.0, bottom: 4.0),
+                    child: TextFormField(
+                        obscureText: true,
+                        controller: passwordController,
+                        decoration: InputDecoration(
+                          labelText: "Password",
+                          prefixIcon: Icon(Icons.lock),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                        ),
+                        validator: (value){
+                          RegExp regex =
+                          RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$');
+                          if (value == null || value.isEmpty)
+                          {
+                            return 'Please enter password';
+                          } else {
+                            if (!regex.hasMatch(value)) {
+                              return 'Enter valid password';
+                            } else {
+                              return " ";
+                            }
+                          }
+                        }
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 30.0, bottom: 15.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.redAccent,
+                  fixedSize: Size(1000, 30),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+                onPressed: () {
+                  if (_formKey.currentState!.validate())
+                  {
+
+                  }
+                },
+                child: const Text('Sign Up'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Already have an account."),
+                  TextButton(
+                        onPressed: (){
+                          Navigator.pop(context);
+                        },
+                        child: const Text(
+                          'Login',
+                        ),
+                      )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
