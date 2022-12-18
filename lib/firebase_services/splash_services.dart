06752e639ba4c9944ef916f1834cfd437b434ed4
Nitesh Kumar Singh <anshukumargautam095@gmail.com>
2022-12-18 17:03:25 +0530

    checks user is already login or not and than take action

--- lib/firebase_services/splash_services.dart
+++ lib/firebase_services/splash_services.dart
@@ -2,14 +2,30 @@ import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase/ui/auth/login_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_firebase/ui/post_screen.dart';
 
 class SplashServics
 {
   void isLogin(BuildContext context)
   {
-    Timer(
-      const Duration(seconds: 3),
-        () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginScreen()))
-    );
+    FirebaseAuth _auth = FirebaseAuth.instance;
+
+    final user = _auth.currentUser;
+
+    if(user != null)
+      {
+        Timer(
+            const Duration(seconds: 3),
+                () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => PostScreen()))
+        );
+      }
+    else
+      {
+        Timer(
+            const Duration(seconds: 3),
+                () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginScreen()))
+        );
+      }
   }
 }
