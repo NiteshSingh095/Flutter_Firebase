df7212aa9e09e69f4a2d9036e2f35b99b3a93edb
Nitesh Kumar Singh <anshukumargautam095@gmail.com>
2022-12-22 01:07:31 +0530

    data can be recovered from realtime database

--- lib/ui/post_screen.dart
+++ lib/ui/post_screen.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase/ui/add_posts.dart';
 import 'package:flutter_firebase/ui/auth/login_screen.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class PostScreen extends StatefulWidget {
   const PostScreen({Key? key}) : super(key: key);
@@ -14,6 +16,8 @@ class _PostScreenState extends State<PostScreen>
 {
 
   final _auth = FirebaseAuth.instance;
+  
+  final ref = FirebaseDatabase.instance.ref("Post");
 
   @override
   Widget build(BuildContext context) {
@@ -28,6 +32,23 @@ class _PostScreenState extends State<PostScreen>
               },
               icon: const Icon(Icons.logout)
           ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: FirebaseAnimatedList(
+                query: ref,
+                defaultChild: Text("Loading"),
+                itemBuilder: (context, snapshot, animation, index)
+                {
+                  return ListTile(
+                    title: Text(snapshot.child("title").value.toString()),
+                    subtitle: Text(snapshot.child("subtitle").value.toString()),
+                  );
+                }
+            ),
+          )
         ],
       ),
       floatingActionButton: Padding(
